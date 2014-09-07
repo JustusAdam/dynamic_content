f837f3c245d3100bd48bcdd65fddf3245470347c
Justus Adam <development@justusadam.com>
2014-09-07 23:25:40 +0200

    encapsulated Component arguments

--- src/core/page.py
+++ src/core/page.py
@@ -5,11 +5,37 @@ class Component:
 
     def __init__(self, title, content='', stylesheets=set(), metatags=set(), scripts=set()):
         self.content = content
-        self.stylesheets = stylesheets
-        self.metatags = metatags
-        self.scripts = scripts
+        self._stylesheets = stylesheets
+        self._metatags = metatags
+        self._scripts = scripts
         self.title = title
 
+    @property
+    def stylesheets(self):
+        return self._stylesheets
+
+    @stylesheets.setter
+    def stylesheets(self, val):
+        if isinstance(val, (list, tuple, str)):
+            self._stylesheets = set(val)
+        else:
+            self._stylesheets = val
+
+    @property
+    def metatags(self):
+        return self._metatags
+
+    @metatags.setter
+    def metatags(self, val):
+        self._metatags = val
+
+    @property
+    def scripts(self):
+        return self._scripts
+
+    @scripts.setter
+    def scripts(self, val):
+        self._scripts = val
 
 class Page(Component):
     def __init__(self, url, title='', content='', stylesheets=set(), metatags=set(), scripts=set(), show_title=True):
