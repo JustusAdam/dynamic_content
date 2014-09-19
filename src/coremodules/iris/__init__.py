5908456557f45a91f57d8d9f56c8d0045e69baf7
Justus Adam <development@justusadam.com>
2014-09-19 15:30:24 +0200

    added one more initial page, added some xkcd to the welcome page

--- src/coremodules/iris/__init__.py
+++ src/coremodules/iris/__init__.py
@@ -38,8 +38,13 @@ def prepare():
     f = dbo.Fields()
     f.init_tables()
     conf = ct.config
+
+    ContentHandlers().add_new('iris', name, path_prefix)
+    ContentTypes().add('article', 'Simple Article', 'iris', 'active')
+    f.add_field_type('body', 'Body', 'article', 'iris')
+
     page_id = ct.add_page(**{k:conf['startpage'][k] for k in ['content_type', 'creator', 'page_title', 'published', 'page_type']})
     f.add_field(table='body', page_id=page_id, path_prefix=conf['startpage']['page_type'], content=conf['startpage']['body'])
-    ContentHandlers().add_new('iris', name, path_prefix)
-    ContentTypes().add('start_type', 'iris', 'active')
-    f.add_field_type('body', 'Body', 'start_type', 'iris')
+
+    page_id = ct.add_page('iris', 'article', 'Wuhuuu', 1, True)
+    f.add_field('body', 'iris', page_id, '<p>More content is good</p><iframe src="http://www.xkcd.com" height="600px" width="600px" seamless></iframe>')
