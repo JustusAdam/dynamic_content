9299bed104f19dbcc0a2d76d7703ff0ed885117a
Justus Adam <development@justusadam.com>
2014-09-21 08:38:18 +0200

    added script to ease manual db operations

---
+++ src/manual_db_editing_env.py
@@ -0,0 +1,37 @@
+from core.database import Database
+from core.database_operations import Alias, Modules, ContentHandlers, ContentTypes
+from coremodules.iris.database_operations import Pages, Fields
+from coremodules.commons_engine.database_operations import MenuOperations
+from coremodules.theme_engine.database_operations import RegionOperations
+
+__author__ = 'justusadam'
+
+info = []
+print('For information on editing objects use print(info)')
+
+db = Database()
+info.append('Database: db')
+
+al_dbo = Alias()
+info.append('Alias editing Object: al_dbo')
+
+mo_dbo = Modules()
+info.append('Modules editing object: mo_dbo')
+
+ch_dbo = ContentHandlers()
+info.append('Content handlers editing object: ch_dbo')
+
+ct_dbo = ContentTypes()
+info.append('Content types editing object: ct_dbo')
+
+fi_dbo = Fields()
+info.append('Fields editing object: fi_dbo')
+
+pa_dbo = Pages()
+info.append('Pages editing object: pa_dbo')
+
+men_dbo = MenuOperations()
+info.append('Menu editing object: men_dbo')
+
+ro_dbo = RegionOperations()
+info.append('Region editing object: ro_dbo')
