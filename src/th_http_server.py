73c3a84cec83ef10f86f15603d1a5ebe223add1e
Justus Adam <development@justusadam.com>
2014-09-21 23:22:10 +0200

    tried to make a non-blocking server, failed. I'll come back to it later

---
+++ src/th_http_server.py
@@ -0,0 +1,15 @@
+"""
+These Server implementations are supposed to handle requests in a non-blocking way, however they don't appear to do so
+I suspect it is because the request handler blocks the output stream somehow.
+I'll leave them like this for now and come back later when I'e figured out how to best handle requests non-blocking.
+"""
+
+import socketserver
+from http import server
+
+__author__ = 'justusadam'
+
+
+class ThreadedHTTPServer(server.HTTPServer, socketserver.ThreadingMixIn): pass
+
+class ForkedHTTPServer(server.HTTPServer, socketserver.ForkingMixIn): pass
