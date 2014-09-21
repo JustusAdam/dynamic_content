40f8b4995659f6ee2ee2b64015732fe988ebd893
Justus Adam <development@justusadam.com>
2014-09-21 11:30:25 +0200

    renamed a column, added prepare()

--- src/coremodules/internationalization/database_operations.py
+++ src/coremodules/internationalization/database_operations.py
@@ -10,34 +10,34 @@ class DisplayNamesOperations(Operations):
 
     _queries = {
         'get_display_name': 'select {language} from display_names where machine_name={machine_name} and source={source};',
-        'edit_display_name': 'update display_names set {language}={value} where machine_name={machine_name} and source={source};',
-        'add_item_il': 'insert into display_names (machine__name, source{languages}) values ({machine_name}, {source}{display_names});'
+        'edit_display_name': 'update display_names set {language}={value} where machine_name={machine_name} and source_table={source_table};',
+        'add_item_il': 'insert into display_names (machine__name, source_table{languages}) values ({machine_name}, {source_table}{display_names});'
     }
 
     _tables = ['display_names']
 
-    def get_display_name(self, item, source, language):
+    def get_display_name(self, item, source_table, language):
         machine_name=escape(item)
-        source=escape(source)
-        self.execute('get_display_name', language=language, machine_name=machine_name, source=source)
+        source_table=escape(source_table)
+        self.execute('get_display_name', language=language, machine_name=machine_name, sourc_tablee=source_table)
         result =  self.cursor.fetchone()
         if result:
             return result[0]
         elif language != baselang:
-            self.execute('get_display_name', language=baselang, machine_name=machine_name, source=source)
+            self.execute('get_display_name', language=baselang, machine_name=machine_name, source_table=source_table)
             result = self.cursor.fetchone()
             if result:
                 return result[0]
         return item
 
-    def edit_display_name(self, item, source, language, value):
-        self.execute('edit_display_name', language=language, value=escape(value), machine_name=escape(item), source=escape(source))
+    def edit_display_name(self, item, source_table, language, value):
+        self.execute('edit_display_name', language=language, value=escape(value), machine_name=escape(item), source_table=escape(source_table))
 
-    def add_item(self, item, source, translations=None):
+    def add_item(self, item, source_table, translations=None):
         languages = ''
         display_names = ''
         if translations:
             for a in translations:
                 languages += ', ' + a[0]
                 display_names += ', ' + escape(a[1])
-        self.execute('add_item_il', languages=languages, machine_name=escape(item), source=escape(source), display_names=display_names)
+        self.execute('add_item_il', languages=languages, machine_name=escape(item), source_table=escape(source_table), display_names=display_names)
