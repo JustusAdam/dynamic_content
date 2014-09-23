0b8423b675cca86fd9bf0f0a99f173c967b387e8
Justus Adam <development@justusadam.com>
2014-09-23 13:20:09 +0200

    fixed menus

--- src/coremodules/commons_engine/menus.py
+++ src/coremodules/commons_engine/menus.py
@@ -5,6 +5,52 @@ from framework.page import Component
 __author__ = 'justusadam'
 
 
+class MenuItem:
+
+    def __init__(self, item_name, display_name, item_path, parent_item, weight):
+        self.item_name = item_name
+        self.display_name = display_name
+        self.item_path = item_path
+        self.parent_item = parent_item
+        self.weight = int(weight)
+        self.children = []
+
+    def render(self, depth=0):
+        return (self.render_self(depth)) + self.render_children(depth+1)
+
+    def render_self(self, depth):
+        if depth == 0:
+            return '<' + self.display_name + '>'
+        else:
+            return '-' * depth + '  ' + self.display_name
+
+    def render_children(self, depth=0):
+        if not self.children:
+            return ''
+        else:
+            return (a.render() for a in self.children)
+
+    def __str__(self):
+        return ''.join(str(a) for a in self.render(0))
+
+
+class HTMLMenuItem(MenuItem):
+
+    def render_self(self, depth):
+        if self.item_path:
+            return ContainerElement(self.display_name, html_type='a', classes={'layer-' + str(depth), 'menu'}, element_id=self.item_name, additionals={'href':self.item_path})
+        else:
+            return ContainerElement(self.display_name, html_type='span', classes={'layer-' + str(depth), 'menu'}, element_id=self.item_name)
+
+    def render_children(self, depth=0):
+        if not self.children:
+            return ''
+        return List(*[a.render(depth) for a in self.children], list_type='ul', item_classes='layer-' + str(depth), classes=['layer-' + str(depth), 'menu'])
+
+    def render(self, depth=0):
+        return self.render_self(depth), self.render_children(depth+1)
+
+
 class MenuHandler(CommonsHandler):
 
     source_table = 'menu_items'
@@ -80,49 +126,3 @@ class MenuHandler(CommonsHandler):
         ul_list = self.menu(HTMLMenuItem).render_children(0)
         ul_list.element_id = self.name
         return Component(self.name, self.wrap_content(ul_list))
-
-
-class MenuItem:
-
-    def __init__(self, item_name, display_name, item_path, parent_item, weight):
-        self.item_name = item_name
-        self.display_name = display_name
-        self.item_path = item_path
-        self.parent_item = parent_item
-        self.weight = int(weight)
-        self.children = []
-
-    def render(self, depth=0):
-        return (self.render_self(depth)) + self.render_children(depth+1)
-
-    def render_self(self, depth):
-        if depth == 0:
-            return '<' + self.display_name + '>'
-        else:
-            return '-' * depth + '  ' + self.display_name
-
-    def render_children(self, depth=0):
-        if not self.children:
-            return ''
-        else:
-            return (a.render() for a in self.children)
-
-    def __str__(self):
-        return ''.join(str(a) for a in self.render(0))
-
-
-class HTMLMenuItem(MenuItem):
-
-    def render_self(self, depth):
-        if self.item_path:
-            return ContainerElement(self.display_name, html_type='a', classes={'layer-' + str(depth), 'menu'}, element_id=self.item_name, additionals={'href':self.item_path})
-        else:
-            return ContainerElement(self.display_name, html_type='span', classes={'layer-' + str(depth), 'menu'}, element_id=self.item_name)
-
-    def render_children(self, depth=0):
-        if not self.children:
-            return ''
-        return List(*[a.render(depth) for a in self.children], list_type='ul', item_classes='layer-' + str(depth), classes=['layer-' + str(depth), 'menu'])
-
-    def render(self, depth=0):
-        return self.render_self(depth), self.render_children(depth+1)
