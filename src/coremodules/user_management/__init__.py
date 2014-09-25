745180feb1e92dd78450dcfb97db229eb12227c3
Justus Adam <development@justusadam.com>
2014-09-25 16:01:55 +0200

    added some setup queries for login common

--- src/coremodules/user_management/__init__.py
+++ src/coremodules/user_management/__init__.py
@@ -25,9 +25,17 @@ def common_handler(item_type, item_name, show_title):
 def prepare():
     from core.database_operations import ContentHandlers
     from .database_operations import UserOperations, SessionOperations
+    from coremodules.theme_engine.database_operations import RegionOperations
+    from coremodules.internationalization.database_operations import DisplayNamesOperations
     cho = ContentHandlers()
     so = SessionOperations()
     uo = UserOperations()
+    ro = RegionOperations()
+    dn = DisplayNamesOperations()
     so.init_tables()
     uo.init_tables()
+
     cho.add_new('login', 'user_management', login_prefix)
+    ro.add_item('login', 'sidebar_left', 0, 'default_theme')
+    ro.add_item_conf('login', 'login', 'user_management')
+    dn.add_item('login', 'login', {'english': 'User Login'})
