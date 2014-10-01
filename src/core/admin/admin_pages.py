a126c4cbccbe18197db7dcd80540592b8106e981
Justus Adam <development@justusadam.com>
2014-10-01 18:51:04 +0200

    implementing admin pages

--- src/core/admin/admin_pages.py
+++ src/core/admin/admin_pages.py
@@ -1,5 +1,6 @@
 from core.handlers import PageContent
 from .database_operations import AdminOperations
+from framework.html_elements import ContainerElement, List
 
 __author__ = 'justusadam'
 
@@ -8,10 +9,69 @@ class Overview(PageContent):
 
   def __init__(self, url, parent_handler):
     super().__init__(url, parent_handler)
+    self.ops = AdminOperations()
 
   def process_content(self):
+    return self.render_categories(self.categories())
+
+  def get_subcategories(self):
+    return self.ops.get_subcategories()
+
+  def get_categories(self):
+    return self.ops.get_categories()
+
+  def render_categories(self, *subcategories):
+    subcategories = [a.render() for a in subcategories]
+    if len(subcategories) == 1:
+      return ContainerElement(*subcategories)
+    else:
+      half = len(subcategories) / 2
+      return ContainerElement(
+        ContainerElement(*subcategories[:half]),
+        ContainerElement(*subcategories[half:])
+      )
+
+  def categories(self):
+    c = self.get_categories()
+    e = self.subcategories()
+    mapping = {}
+    for item in e:
+      if item.name in mapping:
+        mapping[item.name].append(item)
+      else:
+        mapping[item.name] = [item]
+    return [Category(machine_name, display_name, category, mapping[machine_name]) for (machine_name, display_name, category) in c]
+
+  def subcategories(self):
+    return [Category(machine_name, display_name, category, None) for (machine_name, display_name, category) in self.get_subcategories()]
+
+
+class CategoryPage(Overview):
+
+  def render_categories(self, *subcategories):
     pass
 
-  def get_elements(self):
-    ops = AdminOperations()
-    pass
+  def categories(self):
+    return Category()
+
+
+class Category:
+
+  def __init__(self, name, display_name, parent, sub):
+    self.name = name
+    self.display_name = display_name
+    self.sub = sub
+
+  def render(self, url_base):
+    title = ContainerElement(
+        self.display_name, html_type='a', additionals={'href': url_base + '/' + self.name}
+      )
+    if not self.sub:
+      return title
+    else:
+      return ContainerElement(
+        title,
+        List(
+          [a.render() for a in self.sub]
+        )
+      )
--- src/core/admin/database_operations.py
+++ src/core/admin/database_operations.py
@@ -1,4 +1,4 @@
-from ..database_operations import Operations
+from ..database_operations import Operations, escape
 
 __author__ = 'justusadam'
 
@@ -6,9 +6,22 @@ __author__ = 'justusadam'
 class AdminOperations(Operations):
   _queries = {
     'mysql': {
-
+      'get_all_categories': 'select machine_name, display_name from admin_categories;',
+      'get_subcategories': 'select machine_name, display_name, category from admin_sections {cond} order by subcategory, weight;'
     }
   }
 
-  def get_items(self):
-    pass
+  def get_categories(self):
+
+    self.execute('get_all_categories')
+    return self.cursor.fetchall()
+
+  def get_subcategories(self, *categories):
+    cond = ''
+    if categories:
+      acc = []
+      for item in categories:
+        acc.append('category=' + escape(item))
+      cond = 'where ' + ' and '.join(acc)
+    self.execute('get_subcategories', cond=cond)
+    return self.cursor.fetchall()
