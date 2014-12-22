780290fd81cbd9fb7a5ce5b0dd380f9b2b457820
JustusAdam <development@justusadam.com>
2014-12-22 09:53:21 +0100

    added json transformer for arbitrary objects

--- dynct/util/rest.py
+++ dynct/util/rest.py
@@ -1,5 +1,21 @@
 __author__ = 'justusadam'
 
+import inspect
+import json
 
-def json_transform(o):
-    pass
+
+def _object_transform(o):
+    items = filter(lambda a: not a.startswith('_'), dir(o))
+    def _items():
+        for k in items:
+            v = getattr(o, k)
+            if not inspect.isfunction(v) and not inspect.ismethod(v) and not inspect.isclass(v):
+                yield k,v
+    return {
+        k : v
+        for k,v in _items()
+
+    }
+
+
+json_transform = lambda a: json.dumps(a, default=_object_transform)
