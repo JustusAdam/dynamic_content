e059f7e919f6a37c1afb1f0835169333bf2ffdfa
JustusAdam <development@justusadam.com>
2015-01-17 14:58:05 +0100

    added 'cd' to start.sh
    credit largely goes to @MarauderXtreme
    [ci skip]

--- start.sh
+++ start.sh
@@ -2,4 +2,8 @@
 
 # TODO add shell functions here that adds 'dyc' to the pythonpath?
 
+BASEDIR=`dirname $0`
+
+cd $BASEDIR
+
 python3 ./dyc/application/main.py --port 9012 --host localhost --runlevel testing --pathmap multitable --server wsgi
