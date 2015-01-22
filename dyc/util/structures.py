0cda89efdcec10687cdec4d3b99fb8eca6b566ea
JustusAdam <development@justusadam.com>
2015-01-22 23:40:35 +0100

    added docstring for backbone

--- dyc/util/structures.py
+++ dyc/util/structures.py
@@ -16,4 +16,23 @@ DatabaseArguments = collections.namedtuple(
     ('type', 'user', 'autocommit', 'password', 'name', 'host'))
 
 
-DynamicContent = collections.namedtuple('DynamicContent', ('config', 'context', 'request'))
+"""
+Backbone datastructure for dynamic_content.
+
+Mainly consists (after instantiation by the application) of two dictionaries:
+    context:
+        global values accessible to the view/template during the rendering process
+
+    config:
+        dynamic store for configuration variables used by middleware,
+        handlers, decorators etc.
+        allows for dynamic interaction of mostly intermediate/middleware
+        software and dynamic configuration.
+        (used to be done by model attributes, but this seemed cleaner)
+
+    request:
+        information about the current request
+        (amongst other things 'client' can be found here)
+"""
+DynamicContent = collections.namedtuple('DynamicContent',
+    ('config', 'context', 'request'))
