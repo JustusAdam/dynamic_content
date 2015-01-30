82eb44137a6844cb8809cffd3130d6e6b5ac8e6b
JustusAdam <development@justusadam.com>
2015-01-30 10:47:00 +0100

    convenience hook call methods

--- dycc/hooks.py
+++ dycc/hooks.py
@@ -31,7 +31,7 @@ class HookManager:
 
     def init_hook(self, hook, expected_class=Hook):
         assert isinstance(expected_class, type)
-        assert hook != ''
+        assert hook
         self._hooks[hook] = HookList(hooks=[], expected_class=expected_class)
 
     def register(self, hook, handler, priority=0):
@@ -51,3 +51,51 @@ class HookManager:
 
     def get_hooks(self, hook):
         return self._hooks[hook].hooks
+
+    def blank_call_hooks(self, hook, *args,**kwargs):
+         for h in self.get_hooks(hook):
+             h(*args, **kwargs)
+
+    def blank_call_hooks_with(self, hook, executable, *args, **kwargs):
+        for h in self.get_hooks(hook):
+            executable(h, *args, **kwargs)
+
+    def return_call_hooks(self, hook, *args,**kwargs):
+         for res in self.yield_call_hooks(hook, *args, **kwargs):
+             if not res is None:
+                 return res
+
+    def return_call_hooks_with(self, hook, executable, *args, **kwargs):
+        for res in self.yield_call_hooks_with(hook, executable, *args, **kwargs):
+            if not res is None:
+                return res
+
+    def yield_call_hooks(self, hook, *args,**kwargs):
+        """
+        Call hooks with args, kwargs yielding results
+        :param hook: hook name
+        :param args: args to call hook with
+        :param kwargs: kwargs to call hook with
+        :return: hook(*args, **kwargs)
+        """
+        for h in self.get_hooks(hook):
+            res = h(*args, **kwargs)
+            if not res is None: yield res
+
+    def yield_call_hooks_with(self, hook, executable, *args, **kwargs):
+        """
+        Call executable on hooks with args, kwargs yielding results
+
+        intended usage:
+
+        hook_class.method(hook_instance, *args, **kwargs)
+
+        :param hook: hook name
+        :param executable: executable to call on hook
+        :param args: args to call hook with
+        :param kwargs: kwargs to call hook with
+        :return: executable(hook, *args, **kwargs)
+        """
+        for h in self.get_hooks(hook):
+            res = executable(h, *args, **kwargs)
+            if not res is None: yield res
--- dycm/users/login.py
+++ dycm/users/login.py
@@ -125,8 +125,11 @@ def login(dc_obj, failed):
 @decorator.authorize('access login page')
 def login_post(dc_obj, query):
 
-    for hook in hooks.HookManager.manager().get_hooks('login'):
-        res = hook.handle_login_request(query)
+    manager = hooks.HookManager.manager()
+
+    for res in manager.yield_call_hooks_with(
+            'login', LoginHook.handle_login_request, query
+    ):
         if res is False:
             return ':redirect:/login/failed'
         if res is True:
