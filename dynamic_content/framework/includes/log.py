9c6418859f4a1df60c2eb3cce2f886b6a981259a
JustusAdam <development@justusadam.com>
2015-02-14 20:59:46 +0100

    more docstrings

--- dynamic_content/framework/includes/log.py
+++ dynamic_content/framework/includes/log.py
@@ -1,6 +1,5 @@
 import functools
 from framework.util import time
-import os
 import pathlib
 from framework.component import inject
 
@@ -10,6 +9,7 @@ __version__ = '0.1'
 
 def _cache(func):
     class Cache:
+        """Another level of indirection"""
         __slots__ = 'cached',
 
         def __init__(self, cached):
@@ -18,6 +18,7 @@ def _cache(func):
     _cached = Cache(None)
 
     def inner():
+        """wrapper function"""
         if _cached.cached is None:
             _cached.cached = func()
         return _cached.cached
@@ -28,12 +29,17 @@ def _cache(func):
 @_cache
 @inject('settings')
 def get_path(settings):
+    """
+    Get the full file path to the log
+    :param settings: the settings dict
+    :return: filepath (str)
+    """
     dirpath = (
         pathlib.Path(settings['logfile'])
-        if 'logfile' in settings 
+        if 'logfile' in settings
         else get_default_log_path()
         )
-    
+
     path = dirpath / ''.join(('session-log-', str(time.utcnow()), '.log'))
     if path.exists():
         raise IOError('logfile name exists')
@@ -44,23 +50,34 @@ def get_path(settings):
 
 
 def get_default_log_path():
+    """
+    Get a default path to the log based on the used platform
+    :return:
+    """
     # TODO add platform dependant path
     return pathlib.Path(__file__).parent.resolve()
 
 
 def open_log():
+    """
+    Open the logfile
+    :return: file
+    """
     return open(str(get_path()), mode='a')
 
 
 def write(*args, **kwargs):
+    """Write to the log"""
     with open_log() as log:
         print(*args, file=log, **kwargs)
 
 
 def write_any(*args, **kwargs):
+    """Write with time prepended"""
     write(time.utcnow(), *args, **kwargs)
 
 
+# Write to the log with some standardized information prepended
 print_error = write_error = functools.partial(write_any,     '[ERROR]  ')
 print_info = write_info = functools.partial(write_any,       '[INFO]   ')
 print_warning = write_warning = functools.partial(write_any, '[WARNING]')
