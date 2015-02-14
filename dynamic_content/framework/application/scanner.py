1d4a840ef5583c96a0753749eb03c905795499d8
JustusAdam <development@justusadam.com>
2015-02-14 20:55:34 +0100

    scanner skeleton

--- dynamic_content/framework/application/scanner.py
+++ dynamic_content/framework/application/scanner.py
@@ -5,7 +5,18 @@ __version__ = '0.1'
 
 
 class ScannerHook(hooks.ClassHook):
+    """
+    Hook into the module scanning to handle certain types or names of Symbols
+    """
     hook_name = 'module_scan_hook'
 
     def __call__(self, var_name, var):
         raise NotImplementedError
+
+
+class Scanner(object):
+    """
+    Scanner object to find important functions in hooks
+    """
+    def __call__(self, modules):
+        pass
