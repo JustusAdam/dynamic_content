be8e8d0eb229c3d522532867c4f611b5120c08f4
JustusAdam <development@justusadam.com>
2015-02-17 12:02:13 +0100

    adding tracker

--- dynamic_content/framework/machinery/scanner.py
+++ dynamic_content/framework/machinery/scanner.py
@@ -45,17 +45,18 @@ class Scanner(object):
         self.linker = linker
 
     def scan(self, modules):
+        track = set()
         for module in modules:
             # we go through all the modules first and find all scanner hooks
             # so we can ensure all of them are present
             # at the start of the actual scan later
-            self.find_scanner_hooks(module)
+            self.find_scanner_hooks(module, track)
         for module in modules:
             # now we go through each module
             # calling the hooks we discovered earlier
-            self.find_any(module)
+            self.find_any(module, track)
 
-    def find_any(self, module):
+    def find_any(self, module, track):
         """
         Iter module and call hooks on each symbol
 
@@ -65,33 +66,34 @@ class Scanner(object):
         # we immediately create a frozenset to immutably assign the links
         self.linker[module] = frozenset(
             link
-            for var_name, var in self.iter_module(module)
+            for var_name, var in self.iter_module(module, track)
             for link in ScannerHook.yield_call_hooks(module, var_name, var)
         )
 
-    def find_scanner_hooks(self, module):
+    def find_scanner_hooks(self, module, track):
         """
         Finds instances of ScannerHook and registers them
 
         :param module: module object
         :return:
         """
-        for var_name, var in self.iter_module(module):
+        for var_name, var in self.iter_module(module, track):
             if not isinstance(var, ScannerHook):
                 continue
             elif var not in ScannerHook.get_hooks():
                 var.register_instance()
 
     @staticmethod
-    def iter_module(module):
+    def iter_module(module, track):
         """
         A custom generator to only return
         the symbols newly defined in the module
 
         :param module: module object
+        :param track: persistent set that tracks already scanned objects
         :return: yielding symbols/symbol names
         """
-        file = module.__file__
         for var_name, var in vars(module):
-            if not var.__file__ == file:
+            if var not in track:
+                track.add(var)
                 yield var_name, var
