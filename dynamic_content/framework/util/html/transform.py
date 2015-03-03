743e041243654c76cd30e52eb10a716e2e396db3
JustusAdam <development@justusadam.com>
2015-03-03 22:27:35 +0100

    simplified transformation functions

--- dynamic_content/framework/util/html/transform.py
+++ dynamic_content/framework/util/html/transform.py
@@ -1,35 +1,31 @@
+"""Transform stuff to things related to html"""
 import html as _html
 
 
 __author__ = 'Justus Adam'
-__version__ = '0.1'
+__version__ = '0.1.2'
 
 
 def to_html_head(*items):
+    """
+    Layout *items in a manner that resembles the inside of an opening html tag
+
+    :param items: items to transform
+    :return: str
+    """
     return ' '.join(_to_html_head(a) for a in items if a)
 
 
 def _to_html_head(item):
-    return _head_render_map.get(type(item), _str_to_html_head)(item)
-
-
-def _dict_to_html_head(dict_):
-    return ' '.join(_to_html_head(k) + '="' + _to_html_head(v) + '"' for k, v in dict_.items() if v)
-
-
-def _iterable_to_html_head(list_):
-    return ' '.join(_to_html_head(a) for a in list_ if a)
-
-def _str_to_html_head(elem):
-    return _html.escape(str(elem))
-
-
-_head_render_map = {
-    dict: _dict_to_html_head,
-    list: _iterable_to_html_head,
-    tuple: _iterable_to_html_head,
-    int: str,
-    float: str,
-    set: _iterable_to_html_head,
-    str: _str_to_html_head
-}
+    if isinstance(item, str):
+        return _html.escape(item)
+    elif isinstance(item, dict):
+        return ' '.join(
+            '{}="{}"'.format(
+                _to_html_head(k), _to_html_head(v))
+            for k, v in item.items() if v
+        )
+    elif hasattr(item, '__iter__'):
+        return ' '.join(_to_html_head(a) for a in item if a)
+    else:
+        return _html.escape(str(item))
