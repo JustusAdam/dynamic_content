5b8875a912407e2c311a36b360409f6dec7ad35d
JustusAdam <development@justusadam.com>
2015-03-03 22:32:25 +0100

    added some coments explaining my actions

--- dynamic_content/framework/util/html/transform.py
+++ dynamic_content/framework/util/html/transform.py
@@ -18,14 +18,23 @@ def to_html_head(*items):
 
 def _to_html_head(item):
     if isinstance(item, str):
+        # have to test for str first because we
+        # handle arbitrary iterable types later
+        # and str implements the __iter__ method
+        # used to identify iterable types
         return _html.escape(item)
     elif isinstance(item, dict):
+        # we handle dict next because it too implements
+        # the __iter__ method but only iterates keys
+        # and we want keys + values
         return ' '.join(
             '{}="{}"'.format(
                 _to_html_head(k), _to_html_head(v))
             for k, v in item.items() if v
         )
     elif hasattr(item, '__iter__'):
+        # handling arbitrary iterable types
+        # this should include generators
         return ' '.join(_to_html_head(a) for a in item if a)
     else:
         return _html.escape(str(item))
