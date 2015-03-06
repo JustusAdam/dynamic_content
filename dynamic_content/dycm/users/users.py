5db64452b73ef1f1e9c610b7886b114d7625549c
JustusAdam <development@justusadam.com>
2015-03-06 14:16:12 +0100

    more object oriented permission usage

--- dynamic_content/dycm/users/users.py
+++ dynamic_content/dycm/users/users.py
@@ -26,13 +26,28 @@ model.AccessGroup.create_table(fail_silently=True)
 
 # special access groups
 UNKNOWN_GRP = -1  # placeholder - user group undetermined
-CONTROL_GROUP = model.AccessGroup.get_or_create(oid=CONTROL_GROUP_NR, machine_name='control_group')
-GUEST_GRP = model.AccessGroup.get_or_create(oid=1, machine_name='_GUEST_GROUP')  # Not an authenticated User
-AUTH = model.AccessGroup.get_or_create(oid=2, machine_name='_AUTH')  # Default group for users. users that have no particular group assigned to them
+
+CONTROL_GROUP = model.AccessGroup.get_or_create(
+    oid=CONTROL_GROUP_NR, machine_name='control_group'
+)
+
+# Not an authenticated User
+GUEST_GRP = model.AccessGroup.get_or_create(
+    oid=1, machine_name='_GUEST_GROUP'
+)
+
+# Default group for users. users that have no particular group assigned to them
+AUTH = model.AccessGroup.get_or_create(
+    oid=2, machine_name='_AUTH'
+)
 
 # special usernames
 UNKNOWN = -1  # placeholder - user undetermined
-GUEST = model.User.get_or_create(oid=0, username='_GUEST', access_group=GUEST_GRP, email_address='test@test')  # Not a authenticated User
+
+# Not an authenticated User
+GUEST = model.User.get_or_create(
+    oid=0, username='_GUEST', access_group=GUEST_GRP, email_address='test@test'
+)
 
 
 class Permission:
@@ -64,23 +79,27 @@ class Permission:
         )
 
     def assign(self, group):
-        """assign this permission to the given group"""
+        """
+        Assign this permission to the given group
+
+        :param group: group to assign to
+        :return: permission
+        """
+
+        assert isinstance(group, model.AccessGroup)
         if group == CONTROL_GROUP_NR:
             logging.getLogger(__name__).error(
                 'users, permissions, assign_permission: '
                 'cannot assign permissions to control group'
             )
-        elif check_permission(aid=group, permission=self.permission, strict=True):
+        elif self.check(group, strict=True):
             logging.getLogger(__name__).warning(
                 'in users, permissions, assign_permission:',
                 'access group {} already owns permission {}'.format(
                     group, self.permission
                 )
             )
-        elif not check_permission(
-                aid=CONTROL_GROUP_NR,
-                permission=self.permission
-        ):
+        elif not self.check(CONTROL_GROUP):
             logging.getLogger(__name__).warning(
                 'users, permissions, assign_permission:',
                 'permission {} does not exist yet'.format(self.permission)
@@ -89,8 +108,14 @@ class Permission:
             assign_permission(group, self.permission)
         else:
             model.AccessGroupPermission.create(group=group, permission=group.permission)
+        return self
 
     def revoke(self, group):
+        """
+        Revoke this permission from given group
+        :param group:
+        :return:
+        """
         assert isinstance(group, model.AccessGroup)
         if group == CONTROL_GROUP_NR:
             logging.getLogger(__name__).error(
@@ -178,36 +203,66 @@ def acc_grp(user):
         return AUTH
 
 
-def add_acc_grp(name, aid=-1):
-    if aid != -1:
-        model.AccessGroup.create(machine_name=name, oid=aid)
+def add_acc_grp(name, aid=None):
+    """add a new access group"""
+    if aid is None:
+        return model.AccessGroup.create(machine_name=name)
     else:
-        model.AccessGroup.create(machine_name=name)
+        return model.AccessGroup.create(machine_name=name, oid=aid)
 
 
-def check_permission(aid, permission, strict=False):
-    return Permission(permission).check(aid, strict)
+def check_permission(group, permission, strict=False):
+    """check if given group possesses the permission"""
+    return Permission(permission).check(group, strict)
 
 
-def assign_permission(aid, permission):
-    Permission(permission).assign(aid)
+def assign_permission(group, permission):
+    """assign this permission to this group"""
+    Permission(permission).assign(group)
 
 
-def revoke_permission(aid, permission):
-    Permission(permission).revoke(aid)
+def revoke_permission(group, permission):
+    """revoke this permission from this group"""
+    Permission(permission).revoke(group)
 
 
 def new_permission(permission):
-    p = Permission(permission)
-    p.init_in_db()
-    return p
+    """
+    create a new permission with this name
+
+    :param permission: permissions name
+    :return: the new Permission instance
+    """
+    if not isinstance(permission, Permission):
+        permission = Permission(permission)
+    permission.init_in_db()
+    return permission
 
 
 def remove_permission(permission):
+    """
+    completely remove this permission from the database
+    and all its assignments
+
+    :param permission: permission name
+    :return: None
+    """
     Permission(permission).remove()
 
 
 def add_user(username, password, email, first_name='', middle_name='', last_name='', access_group=AUTH):
+    """
+    Create a new user
+
+    :param username: unique username
+    :param password:
+    :param email:
+    :param first_name:
+    :param middle_name:
+    :param last_name:
+    :param access_group:
+    :return: User instance
+    """
     a = model.User.create(
         username=username,
         email_address=email,
@@ -217,29 +272,36 @@ def add_user(username, password, email, first_name='', middle_name='', last_name
         middle_name=middle_name
     )
     passwd, salt = hash_and_new_salt(password)
-    model.UserAuth.create(uid=a,
-                          password=passwd,
-                          salt=salt)
+    model.UserAuth.create(
+        uid=a,
+        password=passwd,
+        salt=salt
+    )
     return a
 
 
 def assign_access_group(user, group):
+    """assign a group to a user"""
     user = get_single_user(user)
     user.access_group = get_acc_grp(group)
     user.save()
+    return user
 
 
 def get_acc_grp(name_or_id:str):
-    return model.AccessGroup.get(oid=int(name_or_id)) if isinstance(name_or_id, (
-    int, float)) or name_or_id.isnumeric() else model.AccessGroup.get(machine_name=name_or_id)
+    """get accesss group from name or id"""
+    if isinstance(name_or_id, (int, float)) or name_or_id.isnumeric():
+        return model.AccessGroup.get(oid=int(name_or_id))
+    else:
+        return model.AccessGroup.get(machine_name=name_or_id)
 
 
 def get_info(selection):
+    """get information about a selection of users"""
     return model.User.select().limit(selection)
 
 
-def get_single_user(uname_or_uid):
-    return get_user(uname_or_uid)
+get_single_user = get_user
 
 
 def edit_user(user_id, **kwargs):
