377d91e8faac827bc1b4a91a6c26df8bd167c1ae
JustusAdam <development@justusadam.com>
2015-03-07 18:26:29 +0100

    object oriented session management

--- dynamic_content/dycm/users/session.py
+++ dynamic_content/dycm/users/session.py
@@ -23,23 +23,63 @@ def new_exp_time():
     return time.utcnow() - datetime.timedelta(seconds=SESSION_LENGTH)
 
 
-def start_session(uid_or_username:str, password):
-    if not authenticate_user(uid_or_username, password):
-        return None
-    user = users.get_single_user(uid_or_username)
-    try:
-        token = model.Session.get(user=user)
-        token = token.token
-    except model.orm.DoesNotExist:
-        token = new_token()
-        model.Session.create(token=token, user=user, expires=new_exp_time())
+class Session:
+    """Session object with utility methods"""
 
-    return binascii.hexlify(token).decode()
+    def __init__(self, user, token=None):
+        assert isinstance(user, model.User)
+        assert isinstance(token, bytes) or token is None
+        self.user = user
+        self.token = token
+
+    @classmethod
+    def start(cls, user, password):
+        assert isinstance(user, model.User)
+        if not authenticate_user(user, password):
+            return None
+        try:
+            token = model.Session.get(user=user).token
+        except model.orm.DoesNotExist:
+            token = new_token()
+            model.Session.create(token=token, user=user, expires=new_exp_time())
+
+        return cls(user, token)
+
+    def str_token(self):
+        return binascii.hexlify(self.token).decode() if self.token is not None else None
+
+    def close(self):
+        model.Session.get(user=self.user).delete()
+        self.token = None
+
+    def is_open(self):
+        return self.token is not None
+
+    @classmethod
+    def validate(cls, token):
+        if isinstance(token, str) and token == SESSION_INVALIDATED:
+            return None
+        if isinstance(token, str):
+            token = binascii.unhexlify(token.encode())
+
+        try:
+            user = model.Session.get(token=token).user
+        except model.orm.DoesNotExist:
+            return None
+        return cls(user, token)
+
+
+def start_session(uid_or_username: str, password):
+    user = (
+        uid_or_username if isinstance(uid_or_username, model.User)
+        else model.User.get(oid=uid_or_username)
+    )
+    return binascii.hexlify(Session.start(user, password).token).decode()
 
 
 def close_session(uid_or_username):
     user = users.get_single_user(uid_or_username)
-    model.Session.get(user=user).delete()
+    Session(user).close()
 
 
 def authenticate_user(username_or_uid, password):
@@ -52,11 +92,4 @@ def authenticate_user(username_or_uid, password):
 
 
 def validate_session(token):
-    if isinstance(token, str) and token == SESSION_INVALIDATED:
-        return None
-    if not isinstance(token, bytes):
-        token = binascii.unhexlify(token.encode())
-    try:
-        return model.Session.get(token=token).user
-    except model.orm.DoesNotExist:
-        return None
+    return Session.validate(token).user
